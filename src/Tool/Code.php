abf5d7bf5ed46534d54f584d255c02e9f46d41f9
yangxm <353125014@qq.com>
2021-12-30 20:13:51 +0800

    加入类

---
+++ src/Tool/Code.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Agl\StarSea\Tool;
+
+class Code
+{
+    /**
+     * TODO 邀请码、订单号，生成
+     * DATE: 2021/12/30
+     * Author: Yxm
+     */
+
+    public function createCode($num, $digit = 6) {
+        static $sourceString = [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ];
+
+        $code = '';
+        while ($num) {
+            $mod = $num % 9;
+            $num = (int) ($num / 9);
+            $code = "{$sourceString[$mod]}{$code}";
+        }
+        //判断code的长度
+        if (empty($code[$digit])) {
+            $code = str_pad($code, $digit, rand(0,9), STR_PAD_RIGHT);
+        }
+        return $code;
+    }
+}
